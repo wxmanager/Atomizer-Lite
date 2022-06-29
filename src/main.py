14518bd966ef05d5f081999dcd59a7bc943820d5
planetjunkie <alshhh.123.987@gmail.com>
2022-06-30 02:18:37 +0400

    stuff

--- src/main.py
+++ src/main.py
@@ -2,17 +2,14 @@ from imports import *
 from PySide6.QtWidgets import *
 from PySide6.QtCore import QSize
 from PySide6.QtGui import QFont
-
-
-def selectedfile():
-  filename = QFileDialog.getOpenFileName()
-  return filename
-
-filename = "test"
+from PySide6.QtWidgets import *
 
 def mainwin():
+  
+    
+
   app = QApplication([])
-  app.setApplicationName("Atomizer - " + filename)
+  app.setApplicationName("Atomizer - " + "")
   appwin = QWidget()
   
   appwin.setMinimumSize(QSize(500, 200))
@@ -22,8 +19,28 @@ def mainwin():
   textedit.setLineWrapMode(QTextEdit.LineWrapMode.NoWrap)
   textedit.setFont(QFont("Monospace"))
 
+  savebutton = QPushButton("Save")
+  savebutton.clicked.connect(lambda: QFileDialog.getSaveFileName(None, "Save File", "", "Atomizer Files (*.atomize)")) 
+  
+  def open():
+    QFileDialog.getOpenFileName(None, "Open Atomizer file", "", "Atomizer Files (*.atomize)")
+    textedit.setText(open)
+
+  openbutton = QPushButton("Open")
+  openbutton.clicked.connect(lambda: open() )
+  
+
+
+
+
+  
+
+
   winlayout = QVBoxLayout()
+  
   winlayout.addWidget(textedit)
+  winlayout.addWidget(savebutton)
+  winlayout.addWidget(openbutton)
   appwin.setLayout(winlayout)
   
   appwin.show()
