1d5bfd4bca06805997be7233b1d553e897e92d32
planetjunkie <alshhh.123.987@gmail.com>
2022-06-30 05:52:10 +0400

    changing a few things

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Atomizer
 
-A Minimalistic customizable python based text editor, enjoy the ✨ Minimalism ✨ and customize the editor to your hearts content!
+A Minimalistic customizable python based sticky note thingy!, enjoy the ✨ Minimalism ✨ and customize the notepad to your hearts content! (doesn't store anything/forgets what you wrote once closed)
+
+Why is atomizer a sticky note app?
+- I am working on a app called Atomizer+ which is a full fledge text editor, with tabs line count and other shit!
--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-from imports import *
 from PySide6.QtWidgets import *
 from PySide6.QtCore import QSize
 from PySide6.QtGui import QFont
@@ -6,10 +5,8 @@ from PySide6.QtWidgets import *
 
 def mainwin():
   
-    
-
   app = QApplication([])
-  app.setApplicationName("Atomizer - " + "")
+  app.setApplicationName("Atomizer")
   appwin = QWidget()
   
   appwin.setMinimumSize(QSize(500, 200))
@@ -19,28 +16,10 @@ def mainwin():
   textedit.setLineWrapMode(QTextEdit.LineWrapMode.NoWrap)
   textedit.setFont(QFont("Monospace"))
 
-  savebutton = QPushButton("Save")
-  savebutton.clicked.connect(lambda: QFileDialog.getSaveFileName(None, "Save File", "", "Atomizer Files (*.atomize)")) 
-  
-  def open():
-    QFileDialog.getOpenFileName(None, "Open Atomizer file", "", "Atomizer Files (*.atomize)")
-    textedit.setText(open)
-
-  openbutton = QPushButton("Open")
-  openbutton.clicked.connect(lambda: open() )
-  
-
-
-
-
-  
-
-
   winlayout = QVBoxLayout()
+  winlayout.setContentsMargins(0,0,0,0)
   
   winlayout.addWidget(textedit)
-  winlayout.addWidget(savebutton)
-  winlayout.addWidget(openbutton)
   appwin.setLayout(winlayout)
   
   appwin.show()
